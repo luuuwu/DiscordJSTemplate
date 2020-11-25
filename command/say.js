3eda66d1191238e3dc34d7c22b4522c1731f027d
SilverBotDev(mxnty) <66350968+SilverBot-mxnty@users.noreply.github.com>
2020-11-25 11:56:11 -0600

    Delete say.js

--- command/say.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'say',
-    description: "Says A message inputted",
-    execute(message, args){
-        const sayMessage = args.join(" ")
-        message.delete().catch(err => console.log(err));
-        message.channel.send(sayMessage);
-    }
-}
