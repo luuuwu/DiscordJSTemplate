deaaac665d5bea3710124d8f49f25ba715e5672d
mxnty <66350968+mxnty999@users.noreply.github.com>
2020-12-15 21:35:21 -0600

    main file

--- index.js
+++ index.js
@@ -1,4 +1,103 @@
 const Discord = require('discord.js');
-const Manager = new Discord.ShardingManager('./bot.js');
-Manager.spawn(2); // This example will spawn 2 shards;
-//Make this a seperate file and run this file.
+const client = new Discord.Client();
+const keepAlive = require ('./server')
+const prefix = '!';
+ 
+const fs = require('fs');
+ 
+client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+// increase the limit
+myEmitter.setMaxListeners(999);
+
+for(let i = 0; i < 11; i++) {
+  myEmitter.on('event', _ => console.log(i));
+}
+
+myEmitter.emit('event');
+
+client.once('ready', () => {
+    console.log('SilverBot Is Online!');
+});
+ 
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args);
+    } 
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'say'){
+        client.commands.get('say').execute(message, args);
+    } 
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === '8ball'){
+        client.commands.get('8ball').execute(message, args);
+    } 
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'ban'){
+        client.commands.get('ban').execute(message, args);
+    } 
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'kick'){
+        client.commands.get('kick').execute(message, args);
+    } 
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'mute'){
+        client.commands.get('mute').execute(message, args);
+    } 
+});
+
+
+keepAlive();
+client.login(process.env.TOKEN);
