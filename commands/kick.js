f4a0cf9960ed3362710a3a8bca75d7497e7a5d94
mxnty <66350968+mxnty999@users.noreply.github.com>
2020-12-15 21:42:29 -0600

    kick

--- commands/kick.js
+++ commands/kick.js
@@ -1,40 +1,32 @@
 module.exports = {
-	name: 'kick',
-    description: 'Kick Member',
-    guildOnly: true,
-    execute(message, args) 
-    {
-        if (message.member.hasPermission(['MANAGE_MESSAGES']))
-        {
-            if (!message.mentions.users.size) 
-            {
-                return message.reply('you need to tag a user in order to kick them!');
-            }
-            const taggedUser = message.mentions.members.first();
-            if (taggedUser.hasPermission(['KICK_MEMBERS']))
-            {
-                message.channel.send(taggedUser.displayName + ` can't be kicked`);  
-            }
-            else
-            {
-                taggedUser.kick().then((taggedUser) => 
-                {
-                    tenor.Search.Query("anime kick", "10").then(Results => {
-                        var images = [];
-                        var i = 0;
-                         Results.forEach(Post => {
-                     
-                             images[i] = Post.url;
-                             i++;
-                         
-                         });
-                         const response = images[Math.floor(Math.random() * images.length)];
-                          
+  name: "ban",
+  description: "Ban Member",
+  guildOnly: true,
+  execute(message, args) {
+    if (message.member.hasPermission(["BAN_MEMBERS"])) {
+      if (!message.mentions.users.size) {
+        return message.reply("you need to tag a user in order to ban them!");
+      }
+      const taggedUser = message.mentions.members.first();
+      if (taggedUser.hasPermission(["MANAGE_MESSAGES"])) {
+        message.channel.send(taggedUser.displayName + ` can't be banned`);
+      } else {
+        taggedUser.ban().then((taggedUser) => {
+          tenor.Search.Query("anime ban", "10").then((Results) => {
+            var images = [];
+            var i = 0;
+            Results.forEach((Post) => {
+              images[i] = Post.url;
+              i++;
+            });
+            const response = images[Math.floor(Math.random() * images.length)];
 
-                        message.channel.send(taggedUser.displayName + ` has been kicked` + " " + response);
-                    });
-                })
-            }   
-        }
+            message.channel.send(
+              taggedUser.displayName + ` has been banned` + " " + response
+            );
+          });
+        });
+      }
     }
-}
+  },
+};
